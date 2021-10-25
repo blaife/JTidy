f769c6cc5929223fddc1e630daa71be59debcccb
blaife <2767026270@qq.com>
2021-10-25 19:12:45 +0800

    算法4 - 排序算法 - 选择排序 选择排序相对来说是最简单的一种排序算法，相对的，它的效率也偏低，仅为(N^2)/2

---
+++ src/main/java/study/blaife/algorithms/sort/SelectionSort.java
@@ -0,0 +1,44 @@
+package study.blaife.algorithms.sort;
+
+import study.blaife.algorithms.sort.SortFrame;
+
+/**
+ * @Description: 选择排序
+ *
+ * 1. 找到数组中最小的那个元素
+ * 2. 将它和数组中的第一个元素交换位置
+ * 3. 再次，在剩下的元素中找到最小的元素
+ *
+ * 交换次数： N
+ * 比较次数： (N^2)/2 --> (N-1)+(N-2)+(N-3)+(N-4)+...+3+2+1 = N(N-1)/2 ~ (N^2)/2
+ *
+ * @Author: magd39318
+ * @Date: 2021/10/25 18:51
+ */
+public class SelectionSort extends SortFrame {
+
+    /**
+     * 排序方法
+     * @param a
+     */
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Comparable[] compareable = new Comparable[]{'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E'};
+        sort(compareable);
+        assert isSorted(compareable);
+        show(compareable);
+    }
+}
