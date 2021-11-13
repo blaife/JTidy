058c08bf2ba24d9e750e9c66dfab1892bc1f0e08
blaife <2767026270@qq.com>
2021-11-14 00:53:59 +0800

    程序计时器 - 整理公共类内容，理解其执行逻辑

--- src/main/java/study/blaife/algorithms/util/Stopwatch.java
+++ src/main/java/study/blaife/algorithms/util/Stopwatch.java
@@ -7,17 +7,50 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * @Description: 程序计时器  抄自 Spring
- * @Author: magd39318
+ * @Author: blaife
  * @Date: 2021/10/26 19:56
  */
 public final class Stopwatch {
+
+    /**
+     * 类唯一 id 可手动指定，也可以为空
+     */
     private final String id;
+
+    /**
+     * 是否保存其任务列表
+     * 存在id参数的构造方法设置为true，也可以使用set方法设置
+     */
     private boolean keepTaskList;
+
+    /**
+     * 任务列表
+     */
     private final List<TaskInfo> taskList;
+
+    /**
+     * 任务执行开始时间
+     */
     private long startTimeNanos;
+
+    /**
+     * 当前任务名称
+     */
     private String currentTaskName;
+
+    /**
+     * 最后一次执行的任务对象
+     */
     private Stopwatch.TaskInfo lastTaskInfo;
+
+    /**
+     * 任务总数
+     */
     private int taskCount;
+
+    /**
+     * 所有任务执行总数
+     */
     private long totalTimeNanos;
 
     public Stopwatch() {
@@ -42,6 +75,17 @@ public final class Stopwatch {
         this.start("");
     }
 
+    /**
+     * 启动方法
+     *
+     * 只允许存在一个任务，若重复启动会抛出异常，提醒任务已经在运行中
+     * 若此时不存在任务，则设置任务名称，并设置任务启动时间为当前时间（System.nanoTime() 此方法以纳秒为单位）
+     *
+     * 注意：任务启动名称可以为空字符串
+     *
+     * @param taskName
+     * @throws IllegalStateException
+     */
     public void start(String taskName) throws IllegalStateException {
         if (this.currentTaskName != null) {
             throw new IllegalStateException("Can't start StopWatch: it's already running");
@@ -51,6 +95,17 @@ public final class Stopwatch {
         }
     }
 
+    /**
+     * 停止任务
+     *
+     * 必须存在一个任务，若当前任务不存在，提醒无运行中的任务
+     * 若正常执行，则设置最近执行时常（lastTime）为当前时间 - 启动时间
+     * 累加其总运行时间，并根据是否保存任务列表参数处理当前任务对象
+     * 添加任务总数，设置当前任务名称为 null
+     *
+     *
+     * @throws IllegalStateException
+     */
     public void stop() throws IllegalStateException {
         if (this.currentTaskName == null) {
             throw new IllegalStateException("Can't stop StopWatch: it's not running");
@@ -131,10 +186,18 @@ public final class Stopwatch {
         }
     }
 
+    /**
+     * 简单的总结
+     * @return
+     */
     public String shortSummary() {
         return "StopWatch '" + this.getId() + "': running time = " + this.getTotalTimeNanos() + " ns";
     }
 
+    /**
+     * 排版输出
+     * @return
+     */
     public String prettyPrint() {
         StringBuilder sb = new StringBuilder(this.shortSummary());
         sb.append('\n');
@@ -183,14 +246,28 @@ public final class Stopwatch {
         return sb.toString();
     }
 
+    /**
+     * 毫秒
+     * @param duration
+     * @return
+     */
     private static long nanosToMillis(long duration) {
         return TimeUnit.NANOSECONDS.toMillis(duration);
     }
 
+    /**
+     * 秒
+     * @param duration
+     * @return
+     */
     private static double nanosToSeconds(long duration) {
         return (double)duration / 1.0E9D;
     }
 
+    /**
+     * 运行任务对象
+     * 任务名称，任务执行时长
+     */
     public static final class TaskInfo {
         private final String taskName;
         private final long timeNanos;
