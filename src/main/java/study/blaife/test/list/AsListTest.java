85c15cc5b80cc95443fa344c8fbfc88aacebcaaf
blaife <2767026270@qq.com>
2022-03-11 17:28:53 +0800

    asList 测试

---
+++ src/main/java/study/blaife/test/list/AsListTest.java
@@ -0,0 +1,27 @@
+package study.blaife.test.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * asList 测试
+ *
+ * @author: magd39318
+ * @date: 2022/3/7 13:19
+ */
+public class AsListTest {
+
+    @Test
+    public void asListTest() {
+        List<String> capClearCodeList = Arrays.asList(new String[]{"104", "105", "405"});
+        HashSet set = new HashSet(capClearCodeList);
+        if (set.contains("104")) {
+            System.out.println("104");
+        }
+
+    }
+
+}
