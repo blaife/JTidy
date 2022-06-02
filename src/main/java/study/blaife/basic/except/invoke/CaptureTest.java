89c8274dd1e2e6ae3a1ec809bbf0723e8bbd3a9d
blaife <2767026270@qq.com>
2022-06-02 11:06:36 +0800

    反射相关

---
+++ src/main/java/study/blaife/basic/except/invoke/CaptureTest.java
@@ -0,0 +1,17 @@
+package study.blaife.basic.except.invoke;
+
+import java.util.ArrayList;
+
+/**
+ * 反射中捕获异常案例
+ *
+ * @author: magd39318
+ * @date: 2022/5/17 9:51
+ */
+public class CaptureTest {
+
+    public static void main(String[] args) {
+        ArrayList
+    }
+
+}
---
+++ src/main/java/study/blaife/basic/except/invoke/MyException.java
@@ -0,0 +1,11 @@
+package study.blaife.basic.except.invoke;
+
+/**
+ * 我的自定义异常
+ *
+ * @author: magd39318
+ * @date: 2022/5/17 9:51
+ */
+public class MyException extends RuntimeException {
+
+}
