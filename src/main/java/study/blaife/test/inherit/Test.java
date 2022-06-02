b9c989629334fb6092fac0630fc0cba197582082
blaife <2767026270@qq.com>
2022-06-02 11:17:59 +0800

    传递类型校验

---
+++ src/main/java/study/blaife/test/inherit/Test.java
@@ -0,0 +1,23 @@
+package study.blaife.test.inherit;
+
+/**
+ * 测试
+ *
+ * @author: magd39318
+ * @date: 2022/4/25 10:12
+ */
+public class Test {
+    int a = 1;
+    final int a2 = a;
+
+    private static int x = 1;
+
+    public int geta() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+}
